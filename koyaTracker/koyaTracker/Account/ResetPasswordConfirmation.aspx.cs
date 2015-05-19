72896342ad32ac9fc5743dcbecc3672495923e07
chiranjeevisailfish <chiru.thripura@gmail.com>
2015-05-19 11:15:13 +0530

    webapp1

---
+++ koyaTracker/koyaTracker/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace koyaTracker.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ koyaTracker/koyaTracker/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(koyaTracker.Startup))]
+namespace koyaTracker
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
